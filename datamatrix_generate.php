e5b7038a85de636b1880c8578941eee567d70534
user <user@Dmitry>
2020-12-02 08:34:20 +0300

    Создание проекта.

--- datamatrix_generate.php
+++ datamatrix_generate.php
@@ -1,11 +1,25 @@
+<?php
+include 'datamatrix.php';
+?>
 <html>
     <body>
         <h1>Генерирование Data Matrix</h1>
+        <p><a href="barcode-master/barcode-dmtx-test.html">barcode-dmtx-test.html</a></p>
+        <p><a href="idautomation/idautomation-datamatrix-demo-static.php">idautomation</a></p>
         <?php
         $source = '';
         $source = '475638475849388p_iG765kj&o8';
         ?>
         <p><?=$source ?></p>
-        <p><img src="barcode-master/barcode.php?s=dmtxs&wq=4&d=<?=$source ?>"></p>
+        <p><img src="barcode-master/barcode.php?s=dmtxs&wq=4&d=<?=$source ?>" /></p>
+        <?php
+        require_once 'tcpdf/tcpdf_barcodes_2d.php';
+        
+        $barcodeobj = new TCPDF2DBarcode($source, 'DATAMATRIX');
+
+        // output the barcode as HTML object
+        echo $barcodeobj->getBarcodeHTML(6, 6, 'black');
+        //include './tcpdf/examples/barcodes/example_2d_datamatrix_png.php';
+        ?>
     </body>
 </html>
