6c0d76eec5b639daaab5022d779f746316aefba4
user <user@Dmitry>
2021-02-16 13:23:04 +0300

    CLEditor

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
         <?php
         include 'include/head.php';
         ?>
+        <link href="<?=APPLICATION ?>/cleditor/cleditor.css" rel="stylesheet" rel="stylesheet" />
+        <style>
+            textarea#input {
+                background-color: white;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -11,9 +17,16 @@
         ?>
         <div class="container-fluid">
             <h1>Принт-дизайн</h1>
+            <textarea id="input" name="input">Go ahead, take it for a test drive. Highlight some text and click some buttons.</textarea>
         </div>
         <?php
         include 'include/footer.php';
         ?>
+        <script src='<?=APPLICATION ?>/cleditor/jquery.cleditor.js'></script>
+        <script>
+            $(document).ready(function () {
+                $("#input").cleditor()[0].focus();
+            });
+        </script>
     </body>
 </html>
