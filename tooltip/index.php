92f2b427b008d10531ab518ed7b1fe2b66287d81
user <user@Dmitry>
2024-08-12 09:42:30 +0300

    2024.08.12 - tooltip

--- tooltip/index.php
+++ tooltip/index.php
@@ -5,52 +5,51 @@
         <link href="../css/jquery-ui.css" rel="stylesheet"/>
         <style>
             body { padding: 20px; }
-
-input[type="text"] { margin: 100px 0 0 200px; }
-
-.ui-tooltip {
-    background: #666;
-    color: white;
-    border: none;
-    padding: 0;
-    opacity: 1;
-}
-.ui-tooltip-content {
-    position: relative;
-    padding: 1em;
-}
-.ui-tooltip-content::after {
-    content: '';
-    position: absolute;
-    border-style: solid;
-    display: block;
-    width: 0;
-}
-.right .ui-tooltip-content::after {
-    top: 18px;
-    left: -10px;
-    border-color: transparent #666;
-    border-width: 10px 10px 10px 0;
-}
-.left .ui-tooltip-content::after {
-    top: 18px;
-    right: -10px;
-    border-color: transparent #666;
-    border-width: 10px 0 10px 10px;
-}
-.top .ui-tooltip-content::after {
-    bottom: -10px;
-    left: 72px;
-    border-color: #666 transparent;
-    border-width: 10px 10px 0;    
-}
-.bottom .ui-tooltip-content::after {
-    top: -10px;
-    left: 72px;
-    border-color: #666 transparent;
-    border-width: 0 10px 10px;
-}
-
+            
+            input[type="text"] { margin: 100px 0 0 200px; }
+            
+            .ui-tooltip {
+                background: #666;
+                color: white;
+                border: none;
+                padding: 0;
+                opacity: 1;
+            }
+            .ui-tooltip-content {
+                position: relative;
+                padding: 1em;
+            }
+            .ui-tooltip-content::after {
+                content: '';
+                position: absolute;
+                border-style: solid;
+                display: block;
+                width: 0;
+            }
+            .right .ui-tooltip-content::after {
+                top: 18px;
+                left: -10px;
+                border-color: transparent #666;
+                border-width: 10px 10px 10px 0;
+            }
+            .left .ui-tooltip-content::after {
+                top: 18px;
+                right: -10px;
+                border-color: transparent #666;
+                border-width: 10px 0 10px 10px;
+            }
+            .top .ui-tooltip-content::after {
+                bottom: -10px;
+                left: 72px;
+                border-color: #666 transparent;
+                border-width: 10px 10px 0;    
+            }
+            .bottom .ui-tooltip-content::after {
+                top: -10px;
+                left: 72px;
+                border-color: #666 transparent;
+                border-width: 0 10px 10px;
+            }
         </style>
     </head>
     <body>
@@ -71,30 +70,30 @@ input[type="text"] { margin: 100px 0 0 200px; }
     <script src="../js/jquery-ui.js"></script>
     <script>
         $('input[type="radio"]').on('change', function() {
-    var className = $(this).val();
-    var position;
-    switch (className) {
-        case 'top':
-            position = { my: 'center bottom', at: 'center top-10' };
-            break;
-        case 'bottom':
-            position = { my: 'center top', at: 'center bottom+10' };
-            break;
-        case 'left':
-            position = { my: 'right center', at: 'left-10 center' };
-            break;
-        case 'right':
-            position = { my: 'left center', at: 'right+10 center' };
-            break;
-    }
-    position.collision = 'none';
-    
-    $('input[type="text"]').tooltip('option', 'position', position);
-    $('input[type="text"]').tooltip('option', 'tooltipClass', className);
-});
-
-$('#options').buttonset();
-$('input[type="text"]').tooltip();
-$('input[value="right"]').trigger('change');
+            var className = $(this).val();
+            var position;
+            switch (className) {
+                case 'top':
+                    position = { my: 'center bottom', at: 'center top-10' };
+                    break;
+                case 'bottom':
+                    position = { my: 'center top', at: 'center bottom+10' };
+                    break;
+                case 'left':
+                    position = { my: 'right center', at: 'left-10 center' };
+                    break;
+                case 'right':
+                    position = { my: 'left center', at: 'right+10 center' };
+                    break;
+            }
+            position.collision = 'none';
+            
+            $('input[type="text"]').tooltip('option', 'position', position);
+            $('input[type="text"]').tooltip('option', 'tooltipClass', className);
+        });
+        
+        $('#options').buttonset();
+        $('input[type="text"]').tooltip();
+        $('input[value="right"]').trigger('change');
     </script>
 </html>
