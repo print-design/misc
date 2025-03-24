e002ef01cc1255a6744d9454d9356552ef54ce50
Дмитрий <@KOMPUTER>
2025-03-24 10:52:49 +0300

    24.03.2025 - Figure

--- figure/index.php
+++ figure/index.php
@@ -10,45 +10,56 @@ include_once '../include/topscripts.php';
         ?>
         <style>
             .figure-point {
-                border: solid 2px blue;
-                height: 5rem;
-                width: 5rem;
+                border: solid .2rem blue;
+                height: 5.2rem;
+                width: 5.2rem;
                 position: absolute;
                 text-align: center;
-                padding-top: .8rem;
             }
             
-            .fp1 { top: 10rem; left: 10rem; }
+            #fp1 { top: 10rem; left: 10rem; }
             
-            .fp2 { top: 10rem; left: 15rem; }
+            #fp2 { top: 10rem; left: 15rem; }
             
-            .fp3 { top: 10rem; left: 20rem; }
+            #fp3 { top: 10rem; left: 20rem; }
             
-            .fp4 { top: 15rem; left: 10rem; }
+            #fp4 { top: 15rem; left: 10rem; }
             
-            .fp5 { top: 15rem; left: 15rem; }
+            #fp5 { top: 15rem; left: 15rem; }
             
-            .fp6 { top: 15rem; left: 20rem; }
+            #fp6 { top: 15rem; left: 20rem; }
             
-            .fp7 { top: 20rem; left: 10rem; }
+            #fp7 { top: 20rem; left: 10rem; }
             
-            .fp8 { top: 20rem; left: 15rem; }
+            #fp8 { top: 20rem; left: 15rem; }
             
-            .fp9 { top: 20rem; left: 20rem; }
+            #fp9 { top: 20rem; left: 20rem; }
         </style>
     </head>
     <body>
         <h1>Фигура</h1>
         <div>
-            <div class="figure-point fp1">1</div>
-            <div class="figure-point fp2">2</div>
-            <div class="figure-point fp3">3</div>
-            <div class="figure-point fp4">4</div>
-            <div class="figure-point fp5">5</div>
-            <div class="figure-point fp6">6</div>
-            <div class="figure-point fp7">7</div>
-            <div class="figure-point fp8">8</div>
-            <div class="figure-point fp9">9</div>
+            <div class="figure-point" id="fp1"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 1);"></div></div>
+            <div class="figure-point" id="fp2"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 2);"></div></div>
+            <div class="figure-point" id="fp3"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 3);"></div></div>
+            <div class="figure-point" id="fp4"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 4);"></div></div>
+            <div class="figure-point" id="fp5"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 5);"></div></div>
+            <div class="figure-point" id="fp6"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 6);"></div></div>
+            <div class="figure-point" id="fp7"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 7);"></div></div>
+            <div class="figure-point" id="fp8"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 8);"></div></div>
+            <div class="figure-point" id="fp9"><div class="figure-drag" style="width: 100%; height: 100%;" onmouseenter="EnterPoint(event, 9);"></div></div>
         </div>
+        <form method="post" id="figure_login">
+            <input type="text" name="figure" id="figure" />
+        </form>
+        <div id="fig2" />
     </body>
+    <script>
+        function EnterPoint(e, number) {
+            if(e.which === 1) {
+                let current = $('input#figure').val();
+                $('input#figure').val(current + number);
+            }
+        }
+    </script>
 </html>
